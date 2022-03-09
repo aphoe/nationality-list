561544f69f44dba0543c863bcbc236c1bcc95672
dinushchathurya <dinushchathurya21@gmail.com>
2022-03-09 20:24:41 +0530

    feat: Finish basic package skelton

--- src/Models/NationalityList.php
+++ src/Models/NationalityList.php
@@ -4,6 +4,7 @@ namespace Dinushchathurya\NationalityList\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Dinushchathurya\NationalityList\Models\NationalityList;
 
 class NationalityList extends Model
 {
