6476a96c88f690002aee72447d90d07c3c515312
dinushchathurya <dinushchathurya21@gmail.com>
2022-03-09 20:49:58 +0530

    feat: Finish basic package skelton

--- src/Models/NationalityList.php
+++ src/Models/NationalityList.php
@@ -2,13 +2,10 @@
 
 namespace Dinushchathurya\NationalityList\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Dinushchathurya\NationalityList\Models\NationalityList;
 
 class NationalityList extends Model
 {
-  use HasFactory;
-  
   protected $guarded = [];
 }
--- src/NationalityServiceProvider.php
+++ src/NationalityServiceProvider.php
@@ -1,14 +1,12 @@
 <?php
-
 namespace Dinushchathurya\NationalityList;
 
 use Illuminate\Support\ServiceProvider;
 
 class NationalityListServiceProvider extends ServiceProvider
 {
-
     public function boot()
-    {   
+    {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
