17f138f19af3902e2dd74915d8b95d693e37ede9
dinushchathurya <dinushchathurya21@gmail.com>
2022-03-10 20:56:14 +0530

    feat: Add all nationalities

--- src/routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['namespace' => 'Dinushchathurya\NationalityList\Http\Controllers'], function () {
-    Route::get('/nationaliities', 'NationalityListController@nationality');
-});
